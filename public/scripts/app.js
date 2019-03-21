c2fa5f77f0e8a815c336be7f1320db7fc1bc70fe
Tess <tess.thornley@gmail.com>
2019-03-21 21:47:40 +0000

    JS updated to calculate how many days ago a tweet was made

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,7 +21,21 @@
 
   function createTweetElement(tweet) {
     let ts = new Date(tweet.created_at);
-    let article = `<article class="tweet"><header><img class="header-img" src=${tweet.user.avatars.small}><h2 class="header-display">${tweet.user.name}</h2><p class="header-p">${tweet.user.handle}</p></header><section><p class="section-p">${escape(tweet.content.text)}</p></section><footer><p class="footer-p">${ts.toDateString()}</p><p class="icons"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i></p></footer></article>`;
+    let todayDate = new Date();
+    let daysAgo = parseInt((todayDate - ts) / (1000 * 60 * 60 * 24)); 
+    let newDate;
+
+    if (daysAgo <= 1) {
+      newDate = "Today";
+    } else if (daysAgo > 1 && daysAgo <= 2) {
+      newDate = "Yesterday";
+    } else if (daysAgo > 2 && daysAgo < 365) {
+      newDate = parseInt((todayDate - ts) / (1000 * 60 * 60 * 24)) + " Days Ago";
+    } else if (daysAgo >= 365) {
+      newDate = parseInt((todayDate - ts) / (1000 * 60 * 60 * 24) / 365) + " Years Ago";
+    }
+
+    let article = `<article class="tweet"><header><img class="header-img" src=${tweet.user.avatars.small}><h2 class="header-display">${tweet.user.name}</h2><p class="header-p">${tweet.user.handle}</p></header><section><p class="section-p">${escape(tweet.content.text)}</p></section><footer><p class="footer-p">${newDate}</p><p class="icons"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i></p></footer></article>`;
     return article;
   }
 
