476485a76486792ba424ff56f363f5063c490bff
Tess <tess.thornley@gmail.com>
2019-03-21 19:05:14 +0000

    MongoDB functionality added to replace the in-memory database

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,26 +1,19 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+    saveTweet: function(newTweet) {
+      db.collection("tweets").insertOne(newTweet);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      db.collection("tweets").find().toArray(callback);
     }
 
   };
