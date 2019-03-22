405525b177e1e30b4370a226c641326e779eeb8d
Tess <tess.thornley@gmail.com>
2019-03-22 19:31:47 +0000

    Docs folder and screenshots added. README.md file updated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,6 @@
  */
 
  $(document).ready(function() {
-
   /* Function that takes in tweets passed from the loadTweets GET request and passes them into the createTweetElement function;
   the returned value of this function is a new tweet article, which will be prepending to the webpage.
   */
@@ -16,7 +15,7 @@
     });
   }
 
-
+  // Preventing cross-site scripting with escape function
   function escape(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
@@ -27,36 +26,28 @@
   inserts the info of the new tweet into the existing article structure, then returns the value.
   */
   function createTweetElement(tweet) {
-    let ts = new Date(tweet.created_at);
-    let todayDate = new Date();
-    let daysAgo = parseInt((todayDate - ts) / (1000 * 60 * 60 * 24)); 
-    let newDate;
-
-    // Logic to display how many days ago a tweet was submitted
-    if (daysAgo <= 1) {
-      newDate = "Today";
-    } else if (daysAgo === 1) {
-      newDate = "Yesterday";
-    } else if (daysAgo >= 2 && daysAgo < 365) {
-      newDate = parseInt((todayDate - ts) / (1000 * 60 * 60 * 24)) + " Days Ago";
-    } else if (daysAgo >= 365) {
-      newDate = parseInt((todayDate - ts) / (1000 * 60 * 60 * 24) / 365) + " Years Ago";
-    }
+    let dateArray = [];
+    // The Date.now() function in the tweets.js file was producing a timestamp that was off by several minutes, added extra time to correct
+    let newDateObj = moment(tweet.created_at).add(1100, 's').toDate();
+    let ts = new Date(newDateObj);
+    dateArray.push(ts.getFullYear(), ts.getMonth(), ts.getDate(), ts.getHours(), ts.getMinutes(), ts.getSeconds());
+    // Moment.js used to calculate relative time from dateArray
+    let daysAgo = moment(dateArray).fromNow();
 
     // Article structure to interpolate new tweet info into
-    let article = `<article class="tweet"><header><img class="header-img" src=${tweet.user.avatars.small}><h2 class="header-display">${tweet.user.name}</h2><p class="header-p">${tweet.user.handle}</p></header><section><p class="section-p">${escape(tweet.content.text)}</p></section><footer><p class="footer-p">${newDate}</p><p class="icons"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i></p></footer></article>`;
+    let article = `<article class="tweet"><header><img class="header-img" src=${tweet.user.avatars.small}><h2 class="header-display">${tweet.user.name}</h2><p class="header-p">${tweet.user.handle}</p></header><section><p class="section-p">${escape(tweet.content.text)}</p></section><footer><p class="footer-p">${daysAgo}</p><p class="icons"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i></p></footer></article>`;
     return article;
   }
 
-  /* Function that makes AJAX GET request to /tweets; once this is received the promise passes
+  /* Function that uses jQuery to make an AJAX GET request to /tweets; once this is received the promise passes
   the data to the renderTweets function to process */
-  function loadTweets(callback) {
+  function loadTweets() {
     $.ajax('/tweets', { method: 'GET' }) 
-      .then(function (data){
-        callback(data);
+      .then(function (data) {
+        renderTweets(data);
     });
   }
   
-  loadTweets(renderTweets);
+  loadTweets();
 
 });
--- server/index.js
+++ server/index.js
@@ -4,22 +4,23 @@
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
-const app           = express();
 const {MongoClient} = require("mongodb");
 const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
 
+const app           = express();
+
+// Middleware to handle URL encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware to serve static files
 app.use(express.static("public"));
 
-
 // Connecting to Mongo and passing the database once connected to factory functions to process
-MongoClient.connect(MONGODB_URI, (err, mongoInstance) => {
+MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
-  let db = mongoInstance;
   // DataHelpers module provides an interface to the database of tweets
   const DataHelpers = require("./lib/data-helpers.js")(db);
   // tweetRoutes is passed DataHelpers object and defines routes it will use to interact with data
@@ -27,7 +28,6 @@ MongoClient.connect(MONGODB_URI, (err, mongoInstance) => {
   // Mount the tweets routes at the "/tweets" path prefix
   app.use("/tweets", tweetsRoutes);
 });
-
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
